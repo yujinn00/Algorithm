e18efa91a5c3aafa7e7e69c8aaccd07be409cb7e
Yujin <qrt365@naver.com>
2023-10-06 09:47:44 +0900

    주유소

--- 그리디 알고리즘/BJ_13305.py
+++ 그리디 알고리즘/BJ_13305.py
@@ -1,5 +1,14 @@
 import sys
 
 n = int(sys.stdin.readline())
-lengths = list(sys.stdin.readline().split())
-prices = list(sys.stdin.readline().split())
+lengths = list(map(int, sys.stdin.readline().split()))
+prices = list(map(int, sys.stdin.readline().split()))
+
+minPrice, minCost = prices[0], 0
+
+for i in range(n - 1):
+  if minPrice > prices[i]:
+    minPrice = prices[i]
+  minCost += minPrice * lengths[i]
+
+print(minCost)
