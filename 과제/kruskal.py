ad5bd3ce5beaabf81b2bae66361a176b2b63a101
Yujin <qrt365@naver.com>
2023-10-10 23:49:03 +0900

    과제 2-1

--- 과제/kruskal.py
+++ 과제/kruskal.py
@@ -1,9 +1,9 @@
 import sys
 
-def find_parent(parent, x):
-  if parent[x] != x:
-    parent[x] = find_parent(parent, parent[x])
-  return parent[x]
+def find_parent(parent, a):
+  if parent[a] != a:
+    parent[a] = find_parent(parent, parent[a])
+  return parent[a]
 
 def union_parent(parent, a, b):
   a = find_parent(parent, a)
@@ -15,35 +15,29 @@ def union_parent(parent, a, b):
     parent[a] = b
 
 def kruskal_mst(graph, n):
-  edges = [(cost, a, b) for a, b, cost in graph]
+  edges = [(weight, a, b) for a, b, weight in graph]
   edges.sort()
 
   parent = [i for i in range(n + 1)]
-  minimum_spanning_tree = []
+  mst = []
 
   for edge in edges:
-    cost, a, b = edge
+    weight, a, b = edge
 
     if find_parent(parent, a) != find_parent(parent, b):
       union_parent(parent, a, b)
-      minimum_spanning_tree.append((a, b, cost))
+      mst.append((a, b, weight))
 
-  return minimum_spanning_tree
+  return mst
 
 v, e = map(int, sys.stdin.readline().split())
 graph = []
 
 for _ in range(e):
-  a, b, cost = map(int, sys.stdin.readline().split())
-  graph.append((a, b, cost))
+  a, b, weight = map(int, sys.stdin.readline().split())
+  graph.append((a, b, weight))
 
-minimum_spanning_tree = kruskal_mst(graph, v)
+mst = kruskal_mst(graph, v)
 
-for edge in minimum_spanning_tree:
+for edge in mst:
   print(edge[0], edge[1], edge[2])
-
-# 크루스칼 알고리즘은 간선을 가중치 순으로 정렬하기 때문에,
-# 간선의 개수에 따라 효율적으로 동작합니다.
-# 그래프의 간선 개수가 많을 때 유용합니다.
-
-# https://8iggy.tistory.com/160
