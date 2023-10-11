865be0f23ce6b8c8215690fa36502a7afdb95fae
Yujin <qrt365@naver.com>
2023-10-11 16:04:20 +0900

    과제 2-2

--- 과제/prim.py
+++ 과제/prim.py
@@ -1,43 +1,38 @@
-import sys
-import heapq
 import collections
+import sys
 
-# 프림 알고리즘
-def prim_mst(graph, start_node):
-  visited[start_node] = 1  # 방문 갱신
-  candidate = graph[start_node]  # 인접 간선 추출
-  heapq.heapify(candidate)  # 우선순위 큐 생성
-  mst = []  # mst
+def prim_mst(graph, start):
+  visited[start] = 1
+  mst = []
 
-  while candidate:
-    weight, u, v = heapq.heappop(candidate)  # 가중치가 가장 적은 간선 추출
-    if visited[v] == 0:  # 방문하지 않았다면
-      visited[v] = 1  # 방문 갱신
-      mst.append((u, v, weight))  # mst에 간선 정보 추가
+  while len(mst) < v - 1:
+    min_weight = float('inf')
+    min_edge = None
 
-      for edge in graph[v]:  # 다음 인접 간선 탐색
-        if visited[edge[2]] == 0:  # 방문한 노드가 아니라면 (순환 방지)
-          heapq.heappush(candidate, edge)  # 우선순위 큐에 간선 삽입
+    for a in graph:
+      if visited[a] == 1:
+        for weight, b in graph[a]:
+          if visited[b] == 0 and weight < min_weight:
+            min_weight = weight
+            min_edge = (a, b, weight)
+
+    if min_edge:
+      a, b, weight = min_edge
+      visited[b] = 1
+      mst.append((a, b, weight))
 
   return mst
 
-n, m = map(int, sys.stdin.readline().split())  # 노드 수, 간선 수
-graph = collections.defaultdict(list)  # 빈 그래프 생성
-visited = [0] * (n + 1)  # 노드의 방문 정보 초기화
+v, e = map(int, sys.stdin.readline().split())
+graph = collections.defaultdict(list)
+visited = [0] * (v + 1)
 
-# 무방향 그래프 생성
-for i in range(m):  # 간선 정보 입력 받기
-  u, v, weight = map(int, input().split())
-  graph[u].append([weight, u, v])
-  graph[v].append([weight, v, u])
+for _ in range(e):
+  a, b, weight = map(int, input().split())
+  graph[a].append((weight, b))
+  graph[b].append((weight, a))
 
-minimum_spanning_tree = prim_mst(graph, 2)  # 시작 노드가 2인 경우
+mst = prim_mst(graph, 2)
 
-for edge in minimum_spanning_tree:
+for edge in mst:
   print(edge[0], edge[1], edge[2])
-
-# 프림 알고리즘은 시작 노드로부터
-# 가장 가까운 노드를 선택하는 방식으로 동작합니다.
-# 그래프의 노드 개수가 많을 때 효율적입니다.
-
-# https://8iggy.tistory.com/159
