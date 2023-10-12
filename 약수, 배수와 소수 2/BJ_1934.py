b36e82a93db65e50ffbbc2d5ac50c33f4a05615b
Yujin <qrt365@naver.com>
2023-10-12 23:31:30 +0900

    최소공배수

---
+++ 약수, 배수와 소수 2/BJ_1934.py
@@ -0,0 +1,15 @@
+import sys
+
+def GCD(a, b):
+  if b == 0:
+    return a
+  else:
+    return GCD(b, a % b)
+
+def LCM(a, b):
+  return a * b / GCD(a, b)
+
+for _ in range(int(sys.stdin.readline())):
+  a, b = map(int, sys.stdin.readline().split())
+
+  print(int(LCM(a, b)))
