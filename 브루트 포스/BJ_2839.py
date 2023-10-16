6507c6be80290154e9d53ca7dcb52df3a4bff530
Yujin <qrt365@naver.com>
2023-10-16 13:03:15 +0900

    설탕 배달

---
+++ 브루트 포스/BJ_2839.py
@@ -0,0 +1,17 @@
+import sys
+
+n = int(sys.stdin.readline())
+count = 0
+
+while True:
+  if n % 5 == 0:
+    count += n // 5
+    print(count)
+    break
+  else:
+    n -= 3
+    count += 1
+
+    if n < 0:
+      print("-1")
+      break
