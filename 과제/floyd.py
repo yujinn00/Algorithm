f869f919661134491e8cab2221fb3399bb685cc3
Yujin <qrt365@naver.com>
2023-11-09 13:55:20 +0900

    과제 3-2

--- 과제/floyd.py
+++ 과제/floyd.py
@@ -1,10 +1,16 @@
 def AllPairsShortest(D, n):
-    for k in range(n):
-        for i in range(n):
-            if i != k:
-                for j in range(n):
-                    if j != k and j != i:
-                        D[i][j] = min(D[i][k] + D[k][j], D[i][j])
+  for k in range(n):
+    for i in range(n):
+      if i != k:
+        for j in range(n):
+          if j != k and j != i:
+            D[i][j] = min(D[i][k] + D[k][j], D[i][j])
+
+  for i in range(n):
+    for j in range(n - 1):
+      if i != j:
+        if i > j:
+          D[i][j] = ''
 
 n = 10
 edges = [[0, 1, 15],
@@ -26,12 +32,13 @@ INF = float("inf")
 dist = [[INF] * n for _ in range(n)]
 
 for i in range(n):
-    dist[i][i] = 0
+  dist[i][i] = 0
 
 for u, v, w in edges:
-    dist[u][v] = w
+  dist[u][v] = w
+  dist[v][u] = w
 
 AllPairsShortest(dist, n)
 
 for d in dist:
-    print(d)
+  print(d)
