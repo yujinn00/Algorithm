20a9bcedb1c3c855d1cce8244550089333362ec7
yujinn00 <100742309+yujinn00@users.noreply.github.com>
2025-01-31 16:18:02 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 84 ms, Memory: 11024 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -7,18 +7,16 @@ bool visited[100001];
 int result[100001];
 int count = 0;
 
-void dfs(int start)
+static void dfs(int node)
 {
-	visited[start] = true;
-	count++;
-	result[start] = count;
+	if (visited[node]) return;
 
-	for (int i = 0; i < graph[start].size(); ++i)
+	visited[node] = true;
+	result[node] = ++count;
+
+	for (int next : graph[node])
 	{
-		if (!visited[graph[start][i]])
-		{
-			dfs(graph[start][i]);
-		}
+		dfs(next);
 	}
 }
 
@@ -28,12 +26,11 @@ int main()
 	std::ios_base::sync_with_stdio(false);
 
 	int n, m, r;
-	int u, v;
-
 	std::cin >> n >> m >> r;
 
 	for (int i = 0; i < m; ++i)
 	{
+		int u, v;
 		std::cin >> u >> v;
 
 		graph[u].push_back(v);
